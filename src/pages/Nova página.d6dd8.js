9c5ba7a52131e594ab706849253f1f8894d75e24
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2025-04-24 20:59:37 +0000

    Initial commit

---
+++ src/pages/Nova página.d6dd8.js
@@ -0,0 +1,6 @@
+// Guia de API: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+	import wixLocation from 'wix-location';
+	wixLocation.to("https://pedroguedelha.github.io/ophell/");
+});
